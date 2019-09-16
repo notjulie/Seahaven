89493dce2e73b5c03504bd9e72d210885ec0921c
notjulie <rasmussenrandy189@gmail.com>
2019-09-16 18:17:36 -0500

    Moved SolverStack to its own file.

--- Solver/Solver.h
+++ Solver/Solver.h
@@ -10,43 +10,10 @@
 
 #include "Solution.h"
 #include "SolverCache.h"
-#include "SolverState.h"
+#include "SolverStack.h"
 
 class SeahavenProblem;
 
-
-class SolverStack {
-public:
-   size_t GetSize(void) const { return stack.size(); }
-   int GetTotalPushCount(void) const { return totalPushes; }
-   bool IsEmpty(void) const { return stack.empty(); }
-   void SetSize(int size) { stack.resize(size); }
-   inline void IncrementPushCount(void) { ++totalPushes; }
-
-   inline SolverState& operator[](int i) { return stack[i]; }
-
-private:
-   std::vector<SolverState>   stack;
-   int totalPushes = 0;
-};
-
-class StackPointer {
-public:
-   StackPointer(SolverStack &_stack)
-      : stack(_stack)
-   {
-   }
-
-   int GetIndex(void) const { return index; }
-   void PushCurrentState(void) { stack[index + 1] = stack[index]; ++index; stack.IncrementPushCount(); }
-
-   inline SolverState& operator*(void) { return stack[index]; }
-   inline SolverState* operator->(void) { return &stack[index]; }
-
-private:
-   SolverStack& stack;
-   int index = 0;
-};
 
 class Solver {
 public:
---
+++ Solver/SolverStack.h
@@ -0,0 +1,41 @@
+
+#ifndef SOLVERSTACK_H
+#define SOLVERSTACK_H
+
+#include "SolverState.h"
+
+class SolverStack {
+public:
+   size_t GetSize(void) const { return stack.size(); }
+   int GetTotalPushCount(void) const { return totalPushes; }
+   bool IsEmpty(void) const { return stack.empty(); }
+   void SetSize(int size) { stack.resize(size); }
+   inline void IncrementPushCount(void) { ++totalPushes; }
+
+   inline SolverState& operator[](int i) { return stack[i]; }
+
+private:
+   std::vector<SolverState>   stack;
+   int totalPushes = 0;
+};
+
+class StackPointer {
+public:
+   StackPointer(SolverStack& _stack)
+      : stack(_stack)
+   {
+   }
+
+   int GetIndex(void) const { return index; }
+   void PushCurrentState(void) { stack[index + 1] = stack[index]; ++index; stack.IncrementPushCount(); }
+
+   inline SolverState& operator*(void) { return stack[index]; }
+   inline SolverState* operator->(void) { return &stack[index]; }
+
+private:
+   SolverStack& stack;
+   int index = 0;
+};
+
+
+#endif
