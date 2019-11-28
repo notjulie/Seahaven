80d93e265f0398fba681cf1a1b5330c9d1bc2b8b
randyrasmussen <randyrasmussen@coxtolstoy.lan>
2019-11-28 07:19:20 -0600

    Added a table.

--- WebGLHaven/public_html/World.js
+++ WebGLHaven/public_html/World.js
@@ -7,50 +7,62 @@
 
 
 function World() {
-    const towerModelNominalHeight = 25.0;
-    
-    this.getColumnPosition = function(column, row) {
-        return {
-            x:(column - 5)*0.5,
-            y:-row * 0.2,
-            z:0.1 * row
-        };
-    }
-    
-    this.getGroundY = function() {
-        return -1.5;
-    }
-    
-    this.getTowerScale = function() {
-        return 0.04;
-    }
-    
-    this.getTowerPosition = function(tower) {
-        switch(tower) {
-            case 0:
-                return {
-                    x:-3.4,
-                    y:-0.7,
-                    z:-0.1
-                };
-            case 1:
-                return {
-                    x:-3.5,
-                    y:-0.4,
-                    z:-0.6
-                };
-                break;
-            case 2:
-            case 3:
-                var result = this.getTowerPosition(3-tower);
-                result.x = -result.x;
-                return result;
-        }
-    }
-    
-    this.getTowerTop = function(tower) {
-        var result = this.getTowerPosition(tower);
-        result.y += towerModelNominalHeight * this.getTowerScale();
-        return result;
-    }
+   const towerModelNominalHeight = 25.0;
+
+   this.getColumnPosition = function (column, row) {
+      return {
+         x: (column - 5) * 0.5,
+         y: -row * 0.2,
+         z: 0.1 * row
+      };
+   }
+
+   this.getGroundY = function () {
+      return -1.5;
+   }
+
+   this.getTableGeometry = function () {
+      var dy = this.getColumnPosition(9,0).y - this.getColumnPosition(9,4).y;
+      var dz = this.getColumnPosition(9,4).z - this.getColumnPosition(9,0).z;
+      return {
+         width:1.05*(this.getColumnPosition(10,0).x - this.getColumnPosition(0,0).x),
+         height:Math.sqrt(dy*dy + dz * dz),
+         bottomY:this.getColumnPosition(9,4).y,
+         bottomZ:this.getColumnPosition(9,4).z,
+         xRotation:-Math.atan2(dz,dy)
+      };
+   }
+
+   this.getTowerScale = function () {
+      return 0.04;
+   }
+
+   this.getTowerPosition = function (tower) {
+      switch (tower) {
+         case 0:
+            return {
+               x: -3.4,
+               y: -0.7,
+               z: -0.1
+            };
+         case 1:
+            return {
+               x: -3.5,
+               y: -0.4,
+               z: -0.6
+            };
+            break;
+         case 2:
+         case 3:
+            var result = this.getTowerPosition(3 - tower);
+            result.x = -result.x;
+            return result;
+      }
+   }
+
+   this.getTowerTop = function (tower) {
+      var result = this.getTowerPosition(tower);
+      result.y += towerModelNominalHeight * this.getTowerScale();
+      return result;
+   }
 }
