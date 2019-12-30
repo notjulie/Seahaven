200149b5106cc00a4bd8a1e5546faf9465bb7127
notjulie <rasmussenrandy189@gmail.com>
2019-12-29 21:13:23 -0600

    Implemented support for creating an object named "solver" that the browser JavaScript can call to execute solver code.  Structurally, anyway.

--- WebGLHaven/WpfHaven/MainWindow.xaml.cs
+++ WebGLHaven/WpfHaven/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CefSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,12 +22,47 @@ namespace WpfHaven
    public partial class MainWindow : Window
    {
       private Server server = new Server();
+      private Solver solver = new Solver();
+
+      public class Solver
+      {
+         public void dobleem()
+         {
+
+         }
+      }
 
       public MainWindow()
       {
          InitializeComponent();
 
+         // add event handlers
+         webBrowser.FrameLoadEnd += WebBrowser_FrameLoadEnd;
+         webBrowser.JavascriptObjectRepository.ResolveObject += JavascriptObjectRepository_ResolveObject;
+
+         // navigate
          webBrowser.Address = new Uri(server.Url, "WebGLHaven/index.html").AbsoluteUri;
+      }
+
+      private void WebBrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+      {
+         // execute some JavaScript that will install our Solver object in the newly loaded frame
+         var solverBinder = new StringBuilder();
+         solverBinder.Append("(async () =>")
+         .Append("{")
+         .Append("await CefSharp.BindObjectAsync('solver');")
+         .Append("})();");
+         e.Frame.ExecuteJavaScriptAsync(solverBinder.ToString());
+      }
+
+      private void JavascriptObjectRepository_ResolveObject(object sender, CefSharp.Event.JavascriptBindingEventArgs e)
+      {
+         switch (e.ObjectName)
+         {
+            case "solver":
+               e.ObjectRepository.Register("solver", solver, true);
+               break;
+         }
       }
    }
 }
