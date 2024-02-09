aae8c97376f600898084203125c86b98fbfd249f
Randy Rasmussen <randy@evergreenresearch.com>
2024-02-08 19:34:43 -0600

    Added a timer.  Amazing

--- PyHaven/QHavenWindow.py
+++ PyHaven/QHavenWindow.py
@@ -1,5 +1,6 @@
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QTimer
 
 class QHavenWindow(QWidget):
     def __init__(self):
@@ -7,5 +8,11 @@ class QHavenWindow(QWidget):
         self.resize(320, 240)  # The resize() method resizes the widget.
         self.setWindowTitle("Hello, Couch!")  # Here we set the title for our window.
 
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.processOneThing)
+        self.timer.start(100)
 
+
+    def processOneThing(self):
+        self.setWindowTitle("Hello, table!")  # Here we set the title for our window.
 
