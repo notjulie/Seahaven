9dbc3067fc1bcfbbfb2eb999f576998182f016d9
Randy Rasmussen <randy@evergreenresearch.com>
2024-02-10 10:44:10 -0600

    More play with PyHaven... proof of concept that QGraphicsView might work for my animation.

--- PyHaven/QHavenWindow.py
+++ PyHaven/QHavenWindow.py
@@ -1,10 +1,20 @@
 
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGraphicsView, QGraphicsScene
 from PyQt5.QtCore import QTimer
 
-class QHavenWindow(QWidget):
+class QHavenWindow(QGraphicsView):
+
     def __init__(self):
-        QWidget.__init__(self)
+        QGraphicsView.__init__(self)
+
+        self.scene = QGraphicsScene(0, 0, 100, 100)
+        self.setScene(self.scene)
+        self.text = self.scene.addText("bleem")
+        self.text.setX(10)
+        self.text.setY(0)
+
+        self.moreText = self.scene.addText("blerg")
+
         self.resize(320, 240)  # The resize() method resizes the widget.
         self.setWindowTitle("Hello, Couch!")  # Here we set the title for our window.
 
@@ -13,6 +23,8 @@ class QHavenWindow(QWidget):
         self.timer.start(100)
 
 
+
     def processOneThing(self):
-        self.setWindowTitle("Hello, table!")  # Here we set the title for our window.
+        #self.setWindowTitle("Hello, table!")  # Here we set the title for our window.
+        self.text.setX(self.text.x() + 1)
 
